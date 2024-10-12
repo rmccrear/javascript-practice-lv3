b7cca93fc58077735e88316ca4c6b496ed398205
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-12 17:01:00 -0500

    Fixed problem 6

--- week-2/settimeout.js
+++ week-2/settimeout.js
@@ -46,14 +46,16 @@ setTimeout( () => {
 // Instructions: Create a series of arrow functions inside setTimeout that count down from 3 to 1 at 1-second intervals, and finally logs "Go!" after 3 seconds.
 // Hint: You'll need to use 3 different setTimeout calls for this task.
 setTimeout(() => {
-  console.log(3);
+  setTimeout(() => {
+    console.log(3);
+  }, 1000);
   setTimeout(() => {
     console.log(2);
-    setTimeout(() => {
-      console.log(1);
-      setTimeout(() => {
-        console.log("GO!");
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}, 1000);
+  }, 2000);
+  setTimeout(() => {
+    console.log(1);
+  }, 3000);
+  setTimeout(() => {
+    console.log("Go!");
+  }, 3000);
+}, 4000);
